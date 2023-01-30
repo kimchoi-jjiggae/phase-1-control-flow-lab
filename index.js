6f884b949726374f1031677b55d632620e07c77f
Michelle <choi.michelle.12@gmail.com>
2023-01-29 21:13:14 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  if (value <= 400){
+    return 'This one is on me!';
+  }
+  if (value > 400 && value<2000){
+    return 'That will be twenty bucks.';
+  }
+  if (value >2000&& value <2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (value>= 2500) {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city==="NYC"?"Ok, sounds good." : "No go.");
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+
+  }
   // Write your code here!
+
 }
